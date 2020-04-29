eda1ecd530d13b454f9eab2693c34b05a3bfd682
prot-dylan <maloney.dylan@gmail.com>
2020-04-29 11:56:57 -0400

    delete corrected

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -5,4 +5,12 @@ class TagsController < ApplicationController
     def index
         @tags = Tag.all
     end
+    def destroy
+
+        @tag = Tag.find(params[:id])
+        @tag.destroy
+        flash.notice = "Tag '#{@tag}' Destroyed!"
+
+        redirect_to :articles
+    end
 end
